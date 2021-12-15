ae3961e98b94a8d2b948a19a6247f116fdc241b6
Boxer-boxer <jorgedasilvarodrigues92@gmail.com>
2021-12-15 00:00:00 -0500

    #1: run serve

--- static/src/vue/dist/js/app.js
+++ static/src/vue/dist/js/app.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d227bf0f32cb1e1b85c3";
+/******/ 	var hotCurrentHash = "b2d511b3ba0730f18c6c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -1422,13 +1422,13 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var C_Us
 
 /***/ 1:
 /*!**********************************************************************************************************************************!*\
-  !*** multi (webpack)/hot/dev-server.js (webpack)-dev-server/client?http://192.168.0.14:8080&sockPath=/sockjs-node ./src/main.js ***!
+  !*** multi (webpack)/hot/dev-server.js (webpack)-dev-server/client?http://192.168.0.14:8081&sockPath=/sockjs-node ./src/main.js ***!
   \**********************************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! C:\Users\jorge\Desktop\timportfolio\vueapp\node_modules\webpack\hot\dev-server.js */"./node_modules/webpack/hot/dev-server.js");
-__webpack_require__(/*! C:\Users\jorge\Desktop\timportfolio\vueapp\node_modules\webpack-dev-server\client\index.js?http://192.168.0.14:8080&sockPath=/sockjs-node */"./node_modules/webpack-dev-server/client/index.js?http://192.168.0.14:8080&sockPath=/sockjs-node");
+__webpack_require__(/*! C:\Users\jorge\Desktop\timportfolio\vueapp\node_modules\webpack-dev-server\client\index.js?http://192.168.0.14:8081&sockPath=/sockjs-node */"./node_modules/webpack-dev-server/client/index.js?http://192.168.0.14:8081&sockPath=/sockjs-node");
 module.exports = __webpack_require__(/*! ./src/main.js */"./src/main.js");
 
 
