215e3faef0229a804fef2153a1cc36296a945205
Jorge Rodrigues <jrodrigues@carpages.ca>
2022-04-29 18:48:34 -0400

    Main: Remove all hot-update files"

--- static/src/vue/dist/chunk-vendors.42f78c20cbd63647fad1.hot-update.js
+++
@@ -1,6 +0,0 @@
-webpackHotUpdate("chunk-vendors",{
-
-/***/ "./node_modules/scroll-watcher/dist/scroll-watcher.min.js":
-false
-
-})
--- static/src/vue/dist/chunk-vendors.5a1b2aeeaeab75420d17.hot-update.js
+++
@@ -1,6 +0,0 @@
-webpackHotUpdate("chunk-vendors",{
-
-/***/ "./node_modules/@fortawesome/free-solid-svg-icons/index.es.js":
-false
-
-})
